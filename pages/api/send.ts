0342bcfaf30366fa33f16b7a05211364151dea08
mitsu-3s <mitsuuu.s3s@gmail.com>
2023-06-26 06:16:10 +0900

    Update send.ts

--- pages/api/send.ts
+++ pages/api/send.ts
@@ -3,7 +3,7 @@ import { NextApiRequest, NextApiResponse } from 'next'
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
         try {
-            // const { latitude, longitude } = req.body
+            // const { latitude, longitude, building } = req.body
             console.log('API', req.body)
             res.status(200).json({ message: 'OK' })
         } catch (e) {
