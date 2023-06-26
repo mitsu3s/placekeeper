cb2e0366022ab09a9fd759b0ad063130a1d4465a
mitsu-3s <mitsuuu.s3s@gmail.com>
2023-06-26 12:38:12 +0900

    Fix api file

---
+++ pages/api/send.ts
@@ -0,0 +1,13 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === 'POST') {
+        try {
+            // const { latitude, longitude, building } = req.body
+            console.log('API', req.body)
+            res.status(200).json({ message: 'OK' })
+        } catch (e) {
+            res.status(500).json({ message: 'Miss' })
+        }
+    }
+}
