c755b264a55f53c4d4a4eb68fecc3a22c90af372
mitsu-3s <mitsuuu.s3s@gmail.com>
2023-07-10 16:04:16 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import React, { useState } from 'react'
 import { Formik, Form, Field, ErrorMessage } from 'formik'
 import axios from 'axios'
 import * as Yup from 'yup'
-import { GetServerSideProps, NextPage } from 'next'
 
 import { PrismaClient } from '@prisma/client'
 
