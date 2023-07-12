54434414a93b13ffa80662b08914b666771778ea
mitsu-3s <mitsuuu.s3s@gmail.com>
2023-07-12 16:23:42 +0900

    Update Map.tsx

--- components/Map.tsx
+++ components/Map.tsx
@@ -4,7 +4,7 @@ import L from 'leaflet'
 import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
 import markerIcon from 'leaflet/dist/images/marker-icon.png'
 import markerShadow from 'leaflet/dist/images/marker-shadow.png'
-import { useRef, useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 
 L.Icon.Default.mergeOptions({
     iconUrl: markerIcon.src,
