ffe0a60248ddbc23927e1a60504b52b402a95c31
mitsu-3s <mitsuuu.s3s@gmail.com>
2023-07-14 15:09:30 +0900

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Session } from 'next-auth'
+import { SessionProvider } from 'next-auth/react'
 
-export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+export default function App({
+    Component,
+    pageProps: { session, ...pageProps },
+}: AppProps<{ session: Session }>) {
+    return (
+        <SessionProvider
+            session={session}
+            // Re-fetch session every 5 minutes
+            refetchInterval={5 * 60}
+            // Re-fetches session when window is focused
+            refetchOnWindowFocus={true}
+        >
+            <Component {...pageProps} />
+        </SessionProvider>
+    )
 }
