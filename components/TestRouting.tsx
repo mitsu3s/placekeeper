a55b5abdda3a5fab6c8299f60d47413f880c7dcc
mitsu-3s <mitsuuu.s3s@gmail.com>
2023-07-16 00:55:44 +0900

    Update TestRouting.tsx

--- components/TestRouting.tsx
+++ components/TestRouting.tsx
@@ -8,10 +8,20 @@ const createRoutineMachineLayer = (props: any) => {
             L.latLng(34.95592012311801, 137.13709831237796),
             L.latLng(34.95191028946883, 137.1674823760987),
         ],
+        // lineOptions: {
+        //     styles: [
+        //         {
+        //             color: 'blue',
+        //             opacity: 0.6,
+        //             weight: 4,
+        //         },
+        //     ],
+        //     extendToWaypoints: true,
+        //     missingRouteTolerance: 1,
+        // },
         addWaypoints: false,
         fitSelectedRoutes: false,
         showAlternatives: false,
-        // draggableWaypoints: false,
     })
 
     return instance
