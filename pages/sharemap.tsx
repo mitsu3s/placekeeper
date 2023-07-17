75a614193efbc091831b56a89b25158ccde073b2
mitsu-3s <mitsuuu.s3s@gmail.com>
2023-07-17 10:07:31 +0900

    Check add sharemap from map

--- pages/sharemap.tsx
+++ pages/sharemap.tsx
@@ -1,5 +1,8 @@
+import React, { useState } from 'react'
 import { useRouter } from 'next/router'
+import { useHash } from '@/libs/useHash'
 import { PrismaClient } from '@prisma/client'
+import PlaceTable from '@/components/PlaceTable'
 
 const prisma = new PrismaClient()
 
@@ -25,11 +28,41 @@ export const getServerSideProps = async (context: any) => {
     }
 }
 
+const centerLatitude = 34.95475940197166
+const centerLongitude = 137.15245841041596
+
 const ShareMapPage = ({ places }: any) => {
-    console.log(places)
+    const [hash, setHash] = useHash()
+    const [centerPosition, setCenterPosition] = useState<[number, number]>([
+        centerLatitude,
+        centerLongitude,
+    ])
+
+    const handlePlaceClick = (placeName: string, lat: number, lng: number) => {
+        setCenterPosition([lat, lng])
+        setHash(formatPlaceNameForHash(placeName))
+    }
+
+    const formatPlaceNameForHash = (placeName: string) => {
+        return placeName.replace(/\s/g, '_')
+    }
     return (
-        <div className="bg-white flex flex-col items-center justify-center h-screen">
-            <p className="text-black">ShareMapPage</p>
+        <div className="bg-white flex flex-col items-center justify-center">
+            <div className="my-2"></div>
+            <div className="w-full flex justify-start">
+                <PlaceTable
+                    places={places}
+                    formatPlaceNameForHash={formatPlaceNameForHash}
+                    handlePlaceClick={handlePlaceClick}
+                />
+                <Map
+                    places={places}
+                    selectedPosition={selectedPosition}
+                    onMapClick={handleMapClick}
+                    center={centerPosition}
+                    className="z-100"
+                />
+            </div>
         </div>
     )
 }
