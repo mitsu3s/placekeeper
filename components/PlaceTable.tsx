5d63f02e29b00200ac050511a2202be8bff6bf9b
mitsu-3s <mitsuuu.s3s@gmail.com>
2023-07-19 12:07:50 +0900

    Update PlaceTable.tsx

--- components/PlaceTable.tsx
+++ components/PlaceTable.tsx
@@ -113,9 +113,9 @@ const PlaceTable = ({ places, formatPlaceNameForHash, handlePlaceClick, updateWa
                                         </th>
                                         <th
                                             scope="col"
-                                            className="px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase"
+                                            className="px-4 py-3 text-right text-xs font-medium text-gray-500 uppercase"
                                         >
-                                            Action
+                                            {/* Action */}
                                         </th>
                                     </tr>
                                 </thead>
@@ -168,13 +168,26 @@ const PlaceTable = ({ places, formatPlaceNameForHash, handlePlaceClick, updateWa
                                                         {place.name}
                                                     </a>
                                                 </td>
-                                                <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                                <td className="px-4 py-3 items-center">
                                                     <a
-                                                        className="text-blue-500 hover:text-blue-700"
+                                                        className="text-black hover:text-blue-700"
                                                         // href="#"
                                                         onClick={() => handleDelete(place.id)}
                                                     >
-                                                        Delete
+                                                        <svg
+                                                            xmlns="http://www.w3.org/2000/svg"
+                                                            fill="none"
+                                                            viewBox="0 0 24 24"
+                                                            strokeWidth={1.5}
+                                                            stroke="currentColor"
+                                                            className="w-5 h-5"
+                                                        >
+                                                            <path
+                                                                strokeLinecap="round"
+                                                                strokeLinejoin="round"
+                                                                d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                                                            />
+                                                        </svg>
                                                     </a>
                                                 </td>
                                             </tr>
