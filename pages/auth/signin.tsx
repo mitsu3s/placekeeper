bc6c1eea816fa0623794b87d8fb1a72c078bbe46
mitsu-3s <mitsuuu.s3s@gmail.com>
2023-07-20 18:27:48 +0900

    Update signin.tsx

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -54,6 +54,7 @@ const Test = () => {
                                         id="email"
                                         type="email"
                                         name="email"
+                                        autoComplete="email"
                                         className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
                                     />
 
