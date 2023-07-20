8341c6c76b2ebf4de2421a91ea7eb60eb113d346
mitsu-3s <mitsuuu.s3s@gmail.com>
2023-07-20 18:06:21 +0900

    Update verify.tsx

--- pages/auth/verify.tsx
+++ pages/auth/verify.tsx
@@ -1,7 +1,17 @@
 const Verify = () => {
     return (
-        <div>
-            <h1>Verify</h1>
+        <div className="bg-white bg-opacity-95 flex items-center justify-center h-screen">
+            <div className="mx-auto max-w-md rounded-xl bg-white border">
+                <div className="px-12 py-10">
+                    <h3 className="text-2xl font-medium text-gray-900 mb-8 text-center">
+                        Check your email
+                    </h3>
+                    <p className="text-md mt-7 text-gray-500 mx-4 text-center">
+                        A sign in link has been sent to <br />
+                        your email address.
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
