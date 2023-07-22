bfc3ee0df05c8445cb35febbdb00d271be6459fc
mitsu-3s <mitsuuu.s3s@gmail.com>
2023-07-22 21:16:24 +0900

    Update PlaceTable.tsx

--- components/PlaceTable.tsx
+++ components/PlaceTable.tsx
@@ -88,7 +88,7 @@ const PlaceTable = ({
                                     onClick={handleSearchIconClick}
                                 >
                                     <svg
-                                        className="h-3.5 w-3.5 text-black"
+                                        className="h-3.5 w-3.5 text-black hover:text-indigo-500"
                                         xmlns="http://www.w3.org/2000/svg"
                                         width="16"
                                         height="16"
