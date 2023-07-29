2b696df68504d3c0439aec83f1e50a0b20dd789a
mitsu-3s <mitsuuu.s3s@gmail.com>
2023-07-29 14:08:04 +0900

    Update useHash.ts

--- utils/useHash.ts
+++ utils/useHash.ts
@@ -5,13 +5,11 @@ export function useHash(): [string, (newHash: string) => void] {
     const router = useRouter()
     const hash = extractHash(router.asPath)
     const setHash = useCallback((newHash: string) => {
-        // ブラウザの履歴に残すなら、ここを router.push に変えれば OK
         router.replace({ hash: newHash }, undefined, { shallow: true })
     }, [])
     return [hash, setHash]
 }
 
-// URL の # 以降の文字列を取り出すユーティリティ
 function extractHash(url: string): string {
     return url.split('#')[1] ?? ''
 }
