3fe2803a83c7358cc3994a7ed2dff105f85538e0
mitsu-3s <mitsuuu.s3s@gmail.com>
2023-08-03 11:31:30 +0900

    Update Map.tsx

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,4 +1,12 @@
-import { MapContainer, Marker, Popup, TileLayer, useMapEvents, useMap } from 'react-leaflet'
+import {
+    MapContainer,
+    Marker,
+    Popup,
+    TileLayer,
+    useMapEvents,
+    useMap,
+    ZoomControl,
+} from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import L from 'leaflet'
 import markerShadow from 'leaflet/dist/images/marker-shadow.png'
@@ -87,11 +95,13 @@ const Map: NextPage<MapProps> = ({
             zoom={14}
             scrollWheelZoom={false}
             style={{ height: '80vh', width: '80%' }}
+            zoomControl={false}
         >
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
+            <ZoomControl position="bottomright" />
             {places &&
                 places.length > 0 &&
                 places.map((place: Place) => (
